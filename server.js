e60b1708a898171328ed8432387f37e355ae3df3
Deuos <74274171+Deuos@users.noreply.github.com>
2023-04-14 14:31:38 -0400

    C:/Program Files/Git/logout created

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ dbConnect()
 const User = require('./Models/user.js')
 //JWT token check
 const auth = require('./Auth/auth.js')
+//port 
 const port = 8080;
 
 //Views
@@ -54,10 +55,12 @@ app.post('/', (req, res, err) => {
                 .then((passwordCheck) => {
                     //check if password matches
                     if (!passwordCheck) {
-                        return res.status(400).send({
-                            message: "Password does not match",
-                            err,
-                        })
+                        //redirects to /
+                        return res.status(400).redirect('')
+                        // return res.status(400).send({
+                        //     message: "Password does not match",
+                        //     err,
+                        // })
                     }
                     //create a JWT token that expires in 24 hours
                     const token = jwt.sign(
@@ -68,29 +71,32 @@ app.post('/', (req, res, err) => {
                         process.env.JWT_TOKEN
                     )
 
-                    //Create cookie set the cookie experies in 24 hours
+                    //Create cookie set the cookie experies in 24 hours and redirects to /profile
                     return res.cookie('access_token', token, {
                         httpOnly: true,
                         maxAge: 24 * 60 * 60 * 1000, // 24 hours
                     }).status(200)
-                    .json({message: "Logged in"});
+                    .redirect('profile');
 
                 })
                 //catches the error if the password doesn't match
                 .catch((err) => {
-                    res.status(400).send({
-                        message: "Password does not match",
-                        err,
-                    })
+                    //redirects to /
+                    res.status(400).redirect('/')
+                    // res.status(400).send({
+                    //     message: "Password does not match",
+                    //     err,
+                    // })
                 })
         })
         //catch error if email does not exist
         .catch((err) => {
-
-            res.status(404).send({
-                message: "Email not found",
-                err,
-            })
+            //redirects to /
+            res.status(404).redirect('/')
+            // res.status(404).send({
+            //     message: "Email not found",
+            //     err,
+            // })
         })
 
 })
@@ -121,32 +127,45 @@ app.post('/register', (req, res) => {
                 .save()
                 .then((result) => {
 
-                    res.status(201).send({
-                        message: "User Created",
-                        result,
-                    })
+                    res.status(201).redirect('/')
+                    // res.status(201).send({
+                    //     message: "User Created",
+                    //     result,
+                    // })
                 })
                 .catch((err) => {
-                    res.status(501).send({
-                        message: "Error creating User",
-                        err
-                    })
+
+                    res.status(501).redirect('register')
+                    // res.status(501).send({
+                    //     message: "Error creating User",
+                    //     err
+                    // })
                 })
         })
         .catch((err) => {
 
-            res.status(500).send({
-                message: "Password not hashed",
-                err
-            })
+            res.status(500).redirect('register')
+            // res.status(500).send({
+            //     message: "Password not hashed",
+            //     err
+            // })
         })
 
+})
+
+//logout
+app.get('/logout', auth, (req, res) => {
+
+    res.clearCookie("access_token")
+    .status(200)
+    .redirect('/')
 })
 
 //auth makes it so no one can access the route without a verfied cookie
 app.get('/profile', auth, async (req, res) => {
 
-    res.send({message: "verfied"})
+    res.render('profile')
+    //res.send({message: "verfied"})
   });
 
 //Localhost 8080
